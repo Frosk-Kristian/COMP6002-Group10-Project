4afa6522dd7bbe7637598df7c17bb7846f0cbacf
Kristian Frossos <55474756+Frosk-Kristian@users.noreply.github.com>
2024-05-02 20:47:42 +0800

    Update rf_wrapper.py

--- Modules/rf_wrapper.py
+++ Modules/rf_wrapper.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import pandas as pd
 import joblib
@@ -6,13 +7,12 @@ from sklearn.ensemble import RandomForestClassifier
 
 def Preprocess(dataframe):
     """
-    Function that performs equivalent preprocessing steps on a dataframe to what was performed in training.
+    Function that performs (relevant) equivalent preprocessing steps on a dataframe to what was performed in training.
 
     Parameters:
         dataframe (pandas.DataFrame): unprocessed dataframe.
     Returns:
-        pandas.DataFrame: processed dataframe if successful.
-        None: if error.
+        pandas.DataFrame: processed dataframe.
     """
     out = dataframe.copy(deep = True)
 
@@ -31,6 +31,36 @@ def Preprocess(dataframe):
 class RF_Model:
     def __init__(self, rf = None, sclr = None):
         self.rf = rf
+        self.sclr = sclr
+
+    def __eprint(self, *args, **kwargs):
+        """
+        Function to print to sys.stderr
+        """
+        print(*args, file=sys.stderr, **kwargs)
 
     def LoadModel(self, fpath):
+        """
+        Function to load a trained model via joblib.
+
+        Parameters:
+            fpath (string): full path to .joblib file, including file name and extension.
+        Returns:
+            True: if successful.
+            False: if unsuccessful.
+        """
+        rf_load = None
+        try:
+            rf_load = joblib.load(fpath)
+        except FileNotFoundError:
+            self.__eprint(f"ERROR: the file \'{fpath}\' was not found.")
+        except:
+            self.__eprint(f"ERROR: an unknown error has occured attempting to call \'joblib.load({fpath})\'")
+            return False
+        else:
+            self.rf = rf_load
+        
+        return True
+
+    def LoadScaler(self, fpath):
         pass
